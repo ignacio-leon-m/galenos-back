e5b1c812362c290039cb814a1dcbf7ab9240f8fe
ignacio-leon-m <ig.leon@duocuc.cl>
2023-10-25 11:28:31 -0300

    first commit, structure

---
+++ src/main/java/com/galenos/galenosbackend/GalenosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.galenos.galenosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalenosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalenosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galenos/galenosbackend/GalenosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galenos.galenosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalenosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
